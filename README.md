1ad263b3196ab8f7cc295dade50263991cd55ab7
= <=>
2022-10-05 21:24:15 -0700

    Formatting, credits, and README.md info

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # PrettyCurrentSong
+PrettyCurrentSong is a small, stylable web app that will display information about the current song playing on your Spotify player.
+
+To configure the application to work with your personal Spotify account:
+
+Change line 137 (or thereabouts) in main.js to your own Spofity client ID. To get your own ID, follow the Getting Started guide here: https://developer.spotify.com/documentation/web-api/quick-start/ (up until Preparing Your Environments).
+
+Use the run_pcs.cmd or run_pcs.sh scripts (Windows and Mac/Linux, respectively) to launch the server.
+
+Launch Spotify and start playing something.
+
+Add a Browser Source to your OBS scene, and set the URL to http://localhost:8888.
+I use a height of 75 and a width of 700, but you do you.  I recommend checking "Shutdown source when not visible" and "Refresh browser when scene becomes active". Initially, you may want to set the height to be bigger because of the next step.
+
+Once you add it (and have the server running!), click the Interact button in OBS. This should open the app in an interactive window, and it should prompt you to sign into Spotify, and authorize the application to access your account. You are only giving the application the permission to read about your current player, and what media it's playing - it is read-only: this app does not and cannot modify any of your account information.
+
+Once you're logged in, you should see the currently playing song.  Resize the control however you want now.
+
+The refresh rate is set to 10 seconds, so there could be up to a 10-second delay in picking up a song change or pause/play state change.
--- public/main.js
+++ public/main.js
@@ -1,147 +1,150 @@
 
-    function generateRandomString(length) {
-        let text = '';
-        const possible =
-            'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+// Heavily based on code from https://github.com/tobika/spotify-auth-PKCE-example
 
-        for (let i = 0; i < length; i++) {
-            text += possible.charAt(Math.floor(Math.random() * possible.length));
-        }
-        return text;
+function generateRandomString(length) {
+    let text = '';
+    const possible =
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
+    return text;
+}
 
-    async function generateCodeChallenge(codeVerifier) {
-        const digest = await crypto.subtle.digest(
-            'SHA-256',
-            new TextEncoder().encode(codeVerifier),
+async function generateCodeChallenge(codeVerifier) {
+    const digest = await crypto.subtle.digest(
+        'SHA-256',
+        new TextEncoder().encode(codeVerifier),
+    );
+
+    return btoa(String.fromCharCode(...new Uint8Array(digest)))
+        .replace(/=/g, '')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_');
+}
+
+function generateUrlWithSearchParams(url, params) {
+    const urlObject = new URL(url);
+    urlObject.search = new URLSearchParams(params).toString();
+
+    return urlObject.toString();
+}
+
+function redirectToSpotifyAuthorizeEndpoint() {
+    const codeVerifier = generateRandomString(64);
+
+    generateCodeChallenge(codeVerifier).then((code_challenge) => {
+        window.localStorage.setItem('code_verifier', codeVerifier);
+        // Redirect to example:
+        // GET https://accounts.spotify.com/authorize?response_type=code&client_id=77e602fc63fa4b96acff255ed33428d3&redirect_uri=http%3A%2F%2Flocalhost&scope=user-follow-modify&state=e21392da45dbf4&code_challenge=KADwyz1X~HIdcAG20lnXitK6k51xBP4pEMEZHmCneHD1JhrcHjE1P3yU_NjhBz4TdhV6acGo16PCd10xLwMJJ4uCutQZHw&code_challenge_method=S256
+
+        window.location = generateUrlWithSearchParams(
+            'https://accounts.spotify.com/authorize',
+            {
+                response_type: 'code',
+                client_id,
+                scope: scope,
+                code_challenge_method: 'S256',
+                code_challenge,
+                redirect_uri,
+            },
         );
 
-        return btoa(String.fromCharCode(...new Uint8Array(digest)))
-            .replace(/=/g, '')
-            .replace(/\+/g, '-')
-            .replace(/\//g, '_');
-    }
+        // If the user accepts spotify will come back to your application with the code in the response query string
+        // Example: http://localhost:8888/?code=NApCCg..BkWtQ&state=profile%2Factivity
+    });
+}
 
-    function generateUrlWithSearchParams(url, params) {
-        const urlObject = new URL(url);
-        urlObject.search = new URLSearchParams(params).toString();
+function exchangeToken(code) {
+    const code_verifier = localStorage.getItem('code_verifier');
 
-        return urlObject.toString();
-    }
+    fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        },
+        body: new URLSearchParams({
+            client_id,
+            grant_type: 'authorization_code',
+            code,
+            redirect_uri,
+            code_verifier,
+        }),
+    })
+        .then(addThrowErrorToFetch)
+        .then((data) => {
+            processTokenResponse(data);
 
-    function redirectToSpotifyAuthorizeEndpoint() {
-        const codeVerifier = generateRandomString(64);
-
-        generateCodeChallenge(codeVerifier).then((code_challenge) => {
-            window.localStorage.setItem('code_verifier', codeVerifier);
-
-            // Redirect to example:
-            // GET https://accounts.spotify.com/authorize?response_type=code&client_id=77e602fc63fa4b96acff255ed33428d3&redirect_uri=http%3A%2F%2Flocalhost&scope=user-follow-modify&state=e21392da45dbf4&code_challenge=KADwyz1X~HIdcAG20lnXitK6k51xBP4pEMEZHmCneHD1JhrcHjE1P3yU_NjhBz4TdhV6acGo16PCd10xLwMJJ4uCutQZHw&code_challenge_method=S256
-
-            window.location = generateUrlWithSearchParams(
-                'https://accounts.spotify.com/authorize',
-                {
-                    response_type: 'code',
-                    client_id,
-                    scope: scope,
-                    code_challenge_method: 'S256',
-                    code_challenge,
-                    redirect_uri,
-                },
-            );
-
-            // If the user accepts spotify will come back to your application with the code in the response query string
-            // Example: http://localhost:8888/?code=NApCCg..BkWtQ&state=profile%2Factivity
-        });
-    }
-
-    function exchangeToken(code) {
-        const code_verifier = localStorage.getItem('code_verifier');
-
-        fetch('https://accounts.spotify.com/api/token', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-            },
-            body: new URLSearchParams({
-                client_id,
-                grant_type: 'authorization_code',
-                code,
-                redirect_uri,
-                code_verifier,
-            }),
+            // clear search query params in the url
+            window.history.replaceState({}, document.title, '/');
         })
-            .then(addThrowErrorToFetch)
-            .then((data) => {
-                processTokenResponse(data);
+        .catch(handleError);
+}
 
-                // clear search query params in the url
-                window.history.replaceState({}, document.title, '/');
-            })
-            .catch(handleError);
+function refreshToken() {
+    fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        },
+        body: new URLSearchParams({
+            client_id,
+            grant_type: 'refresh_token',
+            refresh_token,
+        }),
+    })
+        .then(addThrowErrorToFetch)
+        .then(processTokenResponse)
+        .catch(handleError);
+}
+
+function handleError(error) {
+    console.error(error);
+    mainPlaceholder.innerHTML = errorTemplate({
+        status: error.response.status,
+        message: error.error.error_description,
+    });
+}
+
+async function addThrowErrorToFetch(response) {
+    if (response.ok) {
+        return response.json();
+    } else {
+        throw { response, error: await response.json() };
     }
+}
 
-    function refreshToken() {
-        fetch('https://accounts.spotify.com/api/token', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-            },
-            body: new URLSearchParams({
-                client_id,
-                grant_type: 'refresh_token',
-                refresh_token,
-            }),
-        })
-            .then(addThrowErrorToFetch)
-            .then(processTokenResponse)
-            .catch(handleError);
-    }
+function logout() {
+    localStorage.clear();
+    window.location.reload();
+}
 
-    function handleError(error) {
-        console.error(error);
-        mainPlaceholder.innerHTML = errorTemplate({
-            status: error.response.status,
-            message: error.error.error_description,
-        });
-    }
+function processTokenResponse(data) {
+    access_token = data.access_token;
+    refresh_token = data.refresh_token;
 
-    async function addThrowErrorToFetch(response) {
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw { response, error: await response.json() };
-        }
-    }
+    const t = new Date();
+    expires_at = t.setSeconds(t.getSeconds() + data.expires_in);
 
-    function logout() {
-        localStorage.clear();
-        window.location.reload();
-    }
+    localStorage.setItem('access_token', access_token);
+    localStorage.setItem('refresh_token', refresh_token);
+    localStorage.setItem('expires_at', expires_at);
+}
 
-    function processTokenResponse(data) {
-        access_token = data.access_token;
-        refresh_token = data.refresh_token;
+// Your client id from your app in the spotify dashboard:
+// https://developer.spotify.com/dashboard/applications
+const client_id = 'ec89600e478d4d8aa1a78e6a0a7e6097';
 
-        const t = new Date();
-        expires_at = t.setSeconds(t.getSeconds() + data.expires_in);
+const redirect_uri = 'http://localhost:8888/'; // Your redirect uri
+const scope = 'user-read-playback-state user-read-currently-playing'
 
-        localStorage.setItem('access_token', access_token);
-        localStorage.setItem('refresh_token', refresh_token);
-        localStorage.setItem('expires_at', expires_at);
-    }
-    // Your client id from your app in the spotify dashboard:
-    // https://developer.spotify.com/dashboard/applications
-    const client_id = 'ec89600e478d4d8aa1a78e6a0a7e6097';
-    const redirect_uri = 'http://localhost:8888/'; // Your redirect uri
-    const scope = 'user-read-playback-state user-read-currently-playing'
+// Restore tokens from localStorage
+let access_token = localStorage.getItem('access_token') || null;
+let refresh_token = localStorage.getItem('refresh_token') || null;
+let expires_at = localStorage.getItem('expires_at') || null;
 
-    // Restore tokens from localStorage
-    let access_token = localStorage.getItem('access_token') || null;
-    let refresh_token = localStorage.getItem('refresh_token') || null;
-    let expires_at = localStorage.getItem('expires_at') || null;
-
-(function() {
+(function () {
     // If the user has accepted the authorize request spotify will come back to your application with the code in the response query string
     // Example: http://localhost:8888/?code=NApCCg..BkWtQ&state=profile%2Factivity
     const args = new URLSearchParams(window.location.search);
@@ -152,7 +155,7 @@
         exchangeToken(code);
     } else if (access_token && refresh_token && expires_at) {
         // we are already authorized and reload our tokens from localStorage
-
+        // so we're pretty good here.
     } else {
         // we are not logged in
         redirectToSpotifyAuthorizeEndpoint()
