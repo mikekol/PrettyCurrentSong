0934da43a50a05422ff7159580d9b9bf380bbe85
Mike Kolitz <mikekol@cyberdude.com>
2022-12-22 18:50:14 -0800

    Autosizing fixes

--- public/main.js
+++ public/main.js
@@ -136,7 +136,7 @@ function processTokenResponse(data) {
 // https://developer.spotify.com/dashboard/applications
 const client_id = 'ec89600e478d4d8aa1a78e6a0a7e6097';
 
-const redirect_uri = 'http://localhost:8888/'; // Your redirect uri
+const redirect_uri = 'http://localhost:8888'; // Your redirect uri
 const scope = 'user-read-playback-state user-read-currently-playing'
 
 // Restore tokens from localStorage
