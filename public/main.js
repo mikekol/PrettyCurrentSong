4a8c89dc3b1aa478d135c2c1efafed7ae0970244
Mike Kolitz <kapm47ttv@protonmail.com>
2024-02-05 00:00:39 -0800

    Small updates

--- public/main.js
+++ public/main.js
@@ -134,7 +134,7 @@ function processTokenResponse(data) {
 
 // Your client id from your app in the spotify dashboard:
 // https://developer.spotify.com/dashboard/applications
-const client_id = undefined;
+const client_id = process.env.SPOTIFY_CLIENT_ID | undefined;
 
 const redirect_uri = 'http://localhost:8888'; // Your redirect uri
 const scope = 'user-read-playback-state user-read-currently-playing'
